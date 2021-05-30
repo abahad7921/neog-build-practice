2b889bd111625b64307ec00b8734a3272c8511b3
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2021-05-30 12:41:07 +0530

    initial : created layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [show, setShow] = useState("");
+  // const [showAfter,setShowAfter] = useState("");
+
+  function onChangeHandler() {
+    var val = event.target.value;
+    //console.log(val);
+
+    if (val === "") {
+      setShow("");
+    } else {
+      setShow("now enter the bill amount: ");
+      //setShowAfter("now enter the bill amount");
+    }
+  }
+
+  function showIfTrue() {
+    if (show != "") return <input></input>;
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Manage Cash Program</h1>
+      <h2>Enter amount to see the magic</h2>
+
+      <p>Enter The Total Amount given: </p>
+      <input onChange={onChangeHandler}></input>
+      <p>{show}</p>
+
+      {
+        showIfTrue() //this will show the next bill input if the total amount is given
+      }
     </div>
   );
 }
